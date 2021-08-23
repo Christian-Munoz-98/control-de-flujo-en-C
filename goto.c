4463851c0b04954c00a614911ba5963c050b4b82
Christian Muñoz <chgeovany.15@gmail.com>
2021-08-23 14:48:06 -0500

    goto

---
+++ goto.c
@@ -0,0 +1,14 @@
+for (size_t i = 0; i < count; i++)
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            if (panic)
+                goto errorHandler;
+        }
+        
+    }
+}
+errorHandler: //Todo el codigo para componer tu desastre
+
